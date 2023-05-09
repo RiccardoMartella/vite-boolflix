7553e6f0f2d6dba56d3da800951870ed8a101312
RiccardoMartella <121749204+RiccardoMartella@users.noreply.github.com>
2023-05-09 18:58:01 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+    import NavBarComp from './components/NavBarComp.vue';
+    
+  export default{
+    name: "AppVue",
+    data() {
+        return {};
+    },
+    components: { 
+      NavBarComp,
+     
+     }
+}
+  
+</script>
+
+<template>
+  <NavBarComp />
+</template>
+
+<style lang="scss">
+  @use './style/main.scss';
+</style>
---
+++ src/components/NavBarComp.vue
@@ -0,0 +1,18 @@
+<script >
+    export default{
+      name: "NavBar",
+      data() {
+        return {
+            
+        }
+      },
+}
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped lang="scss">
+
+</style>
